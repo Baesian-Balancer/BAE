e05e58530ca81ade4a2760c6f68e0f44c10944bc
nickioan <nickioan@DESKTOP-Q3TUN5T.localdomain>
2022-02-25 15:43:27 -0800

    added plotting scripts

--- ppo/infer.py
+++ ppo/infer.py
@@ -11,11 +11,9 @@ import core
 import functools
 import os
 from gym_os2r import randomizers
-# from spinup.utils.logx import EpochLogger
-# from spinup.utils.mpi_pytorch import setup_pytorch_for_mpi, sync_params, mpi_avg_grads
-# from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
-
-
+import sys
+sys.path.append("../")
+from plotting import PlotUtils
 def make_env(env_id):
     def make_env_from_id(env_id: str, **kwargs) -> gym.Env:
         return gym.make(env_id, **kwargs)
@@ -90,10 +88,6 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         vf_lr=1e-3, train_pi_iters=80, train_v_iters=80, lam=0.97, max_ep_len=1000000,
         target_kl=0.01, save_freq=10):
 
-    if not os.path.isdir("exp"):
-            os.mkdir("exp")
-
-
     # Random seed 
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -138,13 +132,11 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     for epoch in range(epochs):
         for t in range(local_steps_per_epoch):
             a, v, logp = ac.step(torch.as_tensor(o, dtype=torch.float32),eval=True)
+            plotting.add_action(a)
 
             next_o, r, d, _ = env.step(a)
             ep_ret += r
             ep_len += 1
-
-            # save and log
-            # logger.store(VVals=v)
 
             # Update obs (critical!)
             o = next_o
@@ -161,13 +153,8 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                     _, v, _ = ac.step(torch.as_tensor(o, dtype=torch.float32))
                 else:
                     v = 0
-                # if terminal:
-                #     # only save EpRet / EpLen if trajectory finished
-                #     # logger.store(EpRet=ep_ret, EpLen=ep_len)
                 o, ep_ret, ep_len = env.reset(), 0, 0
         progress_bar.update(1)
-        # if (epoch % save_freq == 0) or (epoch == epochs-1):
-        #     logger.save_state({'env': env}, None)
 
 if __name__ == '__main__':
     import argparse
@@ -178,8 +165,8 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=10000)
     parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--steps', type=int, default=5000)
-    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--steps', type=int, default=1000)
+    parser.add_argument('--epochs', type=int, default=1)
     parser.add_argument('--exp_name', type=str, default='ppo')
     args = parser.parse_args()
 
