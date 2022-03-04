2f79f746a81cd362687b999d4aff3fd1459e3523
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-04 14:18:49 -0800

    change hyper params ppo. more hidden units slightly lower clip ratio

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -276,11 +276,11 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='Monopod-balance-v6')
-    parser.add_argument('--hid', type=int, default=64)
+    parser.add_argument('--hid', type=int, default=128)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--lam', type=float, default=0.97)
-    parser.add_argument('--clip_ratio', type=float, default=0.2)
+    parser.add_argument('--clip_ratio', type=float, default=0.15)
     parser.add_argument('--target_kl', type=float, default=0.01)
     parser.add_argument('--pi_lr', type=float, default=3e-4)
     parser.add_argument('--vf_lr',type=float, default=1e-3)
