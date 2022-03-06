73b5f2442bf366c139a944b558c920648ab2b3dc
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-06 01:59:15 -0800

    change hyper params.

--- ppo/infer.py
+++ ppo/infer.py
@@ -103,8 +103,8 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     # Create actor-critic module
     ac = actor_critic(env.observation_space, env.action_space, **ac_kwargs)
 
-    name = "best_model_step_199999"
-    path = "./exp/207013/"
+    name = "checkpoint_model_step_1509999"
+    path = "./exp/2022_03_05_07_57_55/"
     checkpoint = torch.load(path + name + ".pt")
     ac.load_state_dict(checkpoint['actor_state_dict'])
 
@@ -165,13 +165,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='Monopod-balance-v6')
     # parser.add_argument('--hid', type=int, default=64)
-    parser.add_argument('--hid', type=int, default=128)
+    parser.add_argument('--hid', type=int, default=96)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=10000)
     parser.add_argument('--cpu', type=int, default=4)
     parser.add_argument('--steps', type=int, default=3000)
-    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--exp_name', type=str, default='ppo')
     args = parser.parse_args()
 
