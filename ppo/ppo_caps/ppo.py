2a1e09240e62a33b55a34481bae756e719e097b1
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-07 11:41:35 -0800

    large smoothing reg

--- ppo/ppo_caps/ppo.py
+++ ppo/ppo_caps/ppo.py
@@ -330,7 +330,7 @@ if __name__ == '__main__':
     parser.add_argument('--load_model_path', type=str, default=None)
 
     parser.add_argument('--lam_a', type=float, help='Regularization coeffecient on action smoothness (valid > 0)', default=1)
-    parser.add_argument('--lam_s', type=float, help='Regularization coeffecient on state mapping smoothness (valid > 0)', default=1)
+    parser.add_argument('--lam_s', type=float, help='Regularization coeffecient on state mapping smoothness (valid > 0)', default=10)
     parser.add_argument('--eps_s', type=float, help='Variance coeffecient on state mapping smoothness (valid > 0)', default=0.05)
     parser.add_argument('--lam_o', type=float, help='Regularization coeffecient on observation state mapping smoothness (valid > 0)', default=-1.)
 
