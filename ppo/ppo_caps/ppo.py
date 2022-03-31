bfd03b18cc524fd19e8527741de4d9f095f9480c
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-31 09:39:21 -0700

    .

--- ppo/ppo_caps/ppo.py
+++ ppo/ppo_caps/ppo.py
@@ -412,10 +412,10 @@ if __name__ == '__main__':
     parser.add_argument('--randomizer_on', type=bool, default=True)
 
     parser.add_argument('--lam_ent', type=float, help='Entropy bonus (valid > 0)', default=-.001)
-    parser.add_argument('--lam_ts', type=float, help='Regularization coeffecient on action smoothness (valid > 0)', default=.0002)
+    parser.add_argument('--lam_ts', type=float, help='Regularization coeffecient on action smoothness (valid > 0)', default=.001)
     parser.add_argument('--lam_mdmu', type=float, help='Regularization coeffecient on max action delta (valid > 0)', default=-.1)
     parser.add_argument('--lam_a', type=float, help='Regularization coeffecient on action magnitude (valid > 0)', default=-0.001)
-    parser.add_argument('--lam_sps', type=float, help='Regularization coeffecient on state mapping smoothness (valid > 0)', default=.0005)
+    parser.add_argument('--lam_sps', type=float, help='Regularization coeffecient on state mapping smoothness (valid > 0)', default=.005)
     parser.add_argument('--eps_s', type=float, help='Variance coeffecient on state mapping smoothness (valid > 0)', default=0.001)
     parser.add_argument('--lam_fft', type=float, help='Regularization coeffecient on FFT actions mapping smoothness (valid > 0)', default=-.01)
     parser.add_argument('--lam_rp', type=float, help='Regularization coeffecient on roughness penalty for actions (valid > 0)', default=-0.1)
