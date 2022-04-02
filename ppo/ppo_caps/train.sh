056f551eb69d36068f485bfbff390374aff485c7
Dawson Horvath <horvath.dawson@gmail.com>
2022-04-01 22:36:02 -0700

    .

--- ppo/ppo_caps/train.sh
+++ ppo/ppo_caps/train.sh
@@ -10,5 +10,5 @@ python3 ppo.py \
 --eps_s=0.001 \
 --lam_fft=-1e-2 \
 --lam_rp=-1e-2 \
---load_model_path="exp/2022_04_01_08_17_08/" \
+# --load_model_path="exp/2022_04_01_08_17_08/best_model_step_3279999.pt" \
 --distribution_type="gaussian"
