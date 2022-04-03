94af9db4d2ed4735e71e7e49fac27f08bec6a3ef
Dawson Horvath <horvath.dawson@gmail.com>
2022-04-03 01:37:58 -0700

    .

--- ppo/ppo_caps/train.sh
+++ ppo/ppo_caps/train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 python3 ppo.py \
---epochs=200 \
+--epochs=400 \
 --lam_ent=-1e-3 \
---lam_ts=1e-5 \
---lam_sps=5e-5 \
---hid=96 \
+--lam_ts=5e-6 \
+--lam_sps=25e-6 \
+--hid=128 \
 --lam_mdmu=-.1 \
 --lam_a=-1e-3 \
 --eps_s=0.001 \
 --lam_fft=-1e-2 \
 --lam_rp=-1e-2 \
---load_model_path="./exp/2022_04_02_05_37_14/checkpoint_model_step_3919999.pt" \
+# --load_model_path="./exp/2022_04_02_05_37_14/checkpoint_model_step_3919999.pt" \
 --distribution_type="gaussian"
 # --load_model_path="exp/2022_04_01_08_17_08/best_model_step_3279999.pt" \
